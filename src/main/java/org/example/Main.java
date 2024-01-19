ce5f5b5defcb262e3c1a5fe79bd232b4ae3dec91
Caren-Agala <carenagala324@gmail.com>
2024-01-19 12:43:59 +0300

    Java investment earnings calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
+/*INVESTMENT EARNING CALC
+* enter buying price per share
+* enter closing price on subsequent days
+* calculate aggregate earnings/ losses after each day
+* leave program when closing price is negative*/
 package org.example;
+import java.util.Scanner;
+import java.text.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your buying price:");
+        double buyingPrice = scan.nextDouble();
+        int day = 1;
+        double closingPrice;
+        DecimalFormat df = new DecimalFormat("0.00");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true){
+            System.out.println("Enter the closing price for the day: " + day
+             + ". Any negative value exits the program.");
+            closingPrice = scan.nextDouble();
+            if (closingPrice < 0.0) break;
+            double earnings = closingPrice - buyingPrice;
+            if (earnings > 0.0){
+                System.out.println("After day: " + day + 
+                        ", you earned: " + df.format(earnings) + " per share.");
+            } else if (earnings < 0.0) {
+                System.out.println("After day: " + day +
+                        ", you lost: " + df.format(-earnings) + " per share.");
+            }
+            else{
+                System.out.println("After day: " + day +
+                        ", you earned no  earnings per share.");
+            }
+            day += 1;
+
         }
+        scan.close();
+
     }
 }
--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,2 +1,24 @@
-package org.example;public class TicTacToe {
+package org.example;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args){
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'_', '+', '_', '+', '_'},
+                {' ', '|', ' ', '|', ' '},
+                {'_', '+', '_', '+', '_'},
+                {' ', '|', ' ', '|', ' '}};
+        printGameBoard(gameBoard);
+        Scanner scan = new Scanner(System.in);
+    }
+    public static void printGameBoard(char[][] gameBoard){
+        for(char[] row : gameBoard){
+            for(char c : row){
+              System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
 }
